4ce9f1c6a7611d61873f0b63140b516c68e70b36
Youngsup Kim <idebtor@gmail.com>
2022-01-28 21:41:48 +0900

    up

--- jupyter/dspy/intro/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""
-pythonds3.intro import statement
-"""
-from dspy.intro.oop import Fraction
-
-__all__ = ["Fraction"]
--- jupyter/dspy/searching/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""
-dspy.search import statement
-"""
-from dspy.searching.hash_table import HashTable
-
-__all__ = ["HashTable"]
